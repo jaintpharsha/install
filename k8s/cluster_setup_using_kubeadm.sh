4111619eec342e262ed40b5b3e230eff4ce75268
jaintpharsha <40075520+jaintpharsha@users.noreply.github.com>
2024-05-05 22:19:04 +0530

    Update cluster_setup_using_kubeadm.sh

--- k8s/cluster_setup_using_kubeadm.sh
+++ k8s/cluster_setup_using_kubeadm.sh
@@ -102,7 +102,8 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 [[ -f "$HOME/.kube/config" ]] || echo "     Kubeconfig copied $HOME/.kube/config"
 
 echo -e "\n-------------------------- Install weaveworks network cni --------------------------\n"
-kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+#kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 #kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml 
 
 echo -e "\n---------------------------------- Checking mater node status ---------------------------\n"
