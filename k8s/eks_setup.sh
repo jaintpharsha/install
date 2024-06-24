b7b5a59f3be14b3910b5654da1146ffecae5f028
jaintpharsha <40075520+jaintpharsha@users.noreply.github.com>
2024-06-25 00:40:59 +0530

    Update eks_setup.sh

--- k8s/eks_setup.sh
+++ k8s/eks_setup.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
 install_eksctl() {
-	echo "  Installing eksctl..."
-	# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
-	ARCH=amd64
-	PLATFORM=$(uname -s)_$ARCH
+	if which eksctl &>/dev/null; then
+	    echo "eksctl is already installed...."
+	else
+		echo "  Installing eksctl..."
+		# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+		ARCH=amd64
+		PLATFORM=$(uname -s)_$ARCH
 
-	curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+		curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 
-	# (Optional) Verify checksum
-	curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+		# (Optional) Verify checksum
+		curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
 
-	tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+		tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 
-	sudo mv /tmp/eksctl /usr/local/bin
+		sudo mv /tmp/eksctl /usr/local/bin
+	fi
 }
 
 
 install_kubectl() {
-	echo "  Installing kubectl..."
-	wget https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
-	chmod +x ./kubectl
-	mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-	kubectl version --short --client
+	if which kubectl &>/dev/null; then
+	    echo "kubectl is already installed...."
+	else
+		echo "  Installing kubectl..."
+		wget https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
+		chmod +x ./kubectl
+		mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+		kubectl version --short --client
+	fi
 }
 
 access_key=$(aws configure get aws_access_key_id)
