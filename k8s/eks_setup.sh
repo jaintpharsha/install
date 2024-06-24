8c66f20b5f516a0b329b11c8fe9c820d09a54f9d
jaintpharsha <40075520+jaintpharsha@users.noreply.github.com>
2024-06-25 00:19:54 +0530

    Create eks_setup.sh

---
+++ k8s/eks_setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+install_eksctl() {
+	echo "  Installing eksctl..."
+	# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+	ARCH=amd64
+	PLATFORM=$(uname -s)_$ARCH
+
+	curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+	# (Optional) Verify checksum
+	curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+	tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+	sudo mv /tmp/eksctl /usr/local/bin
+}
+
+
+install_kubectl() {
+	echo "  Installing kubectl..."
+	wget https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
+	chmod +x ./kubectl
+	mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+	kubectl version --short --client
+}
+
+
+read -e -p $'Before continuing with the EKS installation, Need a IAM user with below roles \n    1. AmazonEC2FullAccess \n    2. AWSCloudFormationFullAccess \n    3. EksAllAccess \n    4. IamLimitedAccess (https://eksctl.io/usage/minimum-iam-policies/)\n\nHave you configured the AWS CLI with IAM user with above access using aws configure (y/n): ' choice
+choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+
+
+if [ "$choice" = "yes" ] || [ "$choice" = "y" ]; then
+    install_eksctl
+    install_kubectl
+    eksctl create cluster --name itdefined --version 1.30 --region ap-south-1 --nodegroup-name itdefined-workers --node-type t2.micro --nodes 2 --nodes-min 1 --nodes-max 3 --managed
+else
+    echo "Exiting EKS installation, comeback with IAM user..."
+    # Add any cleanup or exit commands here
+fi
